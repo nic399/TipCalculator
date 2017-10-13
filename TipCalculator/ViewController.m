b07ee6132957d8e4fa0c3a772b9386564ede3cbb
Nicholas Fung <nbfung@sfu.ca>
2017-10-13 13:57:06 -0700

    Automatically calculate tip (no longer require user to tap the Calculate Tip button)

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -20,13 +20,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    
-    
 }
 
-- (IBAction)CalculateTip:(id)sender {
+- (void)CalculateTip {
     if (self.billAmountTextField.text.length > 0 && self.tipPercentageTextField.text.length > 0) {
         double tipAmount = [self.billAmountTextField.text doubleValue] * [self.tipPercentageTextField.text doubleValue] / 100.00;
         self.tipAmountLabel.text = [@"Tip Amount: $" stringByAppendingString:[NSString stringWithFormat:@"%.2f", tipAmount]];
@@ -38,16 +34,18 @@
 
 -(void)textFieldDidEndEditing:(UITextField *)textField {
     self.tipPercentSlider.value = [self.tipPercentageTextField.text doubleValue];
+    [self CalculateTip];
 }
 
 - (IBAction)tipSliderAction:(UISlider *)sender {
     self.tipPercentageTextField.text = [NSString stringWithFormat:@"%.1f", sender.value];
+    [self CalculateTip];
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     NSCharacterSet *decimalNumberCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"1234567890."];
     NSRange testForNonNumbers = [string rangeOfCharacterFromSet:[decimalNumberCharacterSet invertedSet]];
-    
+    [self CalculateTip];
     if (testForNonNumbers.length == 0) {
         if ([textField.text containsString:@"."] && [string containsString:@"."]) { //text field already has a decimal point
             return false;
