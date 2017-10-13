01d96c6f9f374101bcaf198bea3476972231d922
Nicholas Fung <nbfung@sfu.ca>
2017-10-13 11:23:15 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Nicholas Fung on 2017-10-13.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Nicholas Fung on 2017-10-13.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
